a87b15e50ea95aaa06114de6b66e252b1c368735
SimoMassaro52 <simonesimomassaro@gmail.com>
2023-05-11 12:01:28 +0200

    finished details page

--- src/components/VanDetail.jsx
+++ src/components/VanDetail.jsx
@@ -2,13 +2,51 @@ import "../App.css";
 
 //Params function import
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function VanDetail() {
 	const params = useParams();
-	console.log(params);
+	const [van, setVan] = useState(null);
+	useEffect(() => {
+		fetch(`/api/vans/${params.id}`)
+			.then((res) => res.json())
+			.then((data) => setVan(data.vans));
+		//We want to rerun the fetch request only if the id changes
+	}, [params.id]);
+	console.log(van);
 	return (
 		<>
-			<main className="single-van-wrapper">Van details</main>
+			<main className="single-van-wrapper">
+				<Link to={"/vans"} className="back-to">
+					<FontAwesomeIcon icon={faArrowLeft} />
+					<p>Back to all vans</p>
+				</Link>
+				{van ? (
+					<div className="van-detail">
+						<img src={van.imageUrl} />
+						<div className="van-detail-txt">
+							<div>
+								<i className={`van-type ${van.type}`}>
+									{van.type.charAt(0).toUpperCase() + van.type.slice(1)}
+								</i>
+							</div>
+							<h2>{van.name}</h2>
+							<p className="van-price">
+								${van.price}
+								<span>/day</span>
+							</p>
+							<p>{van.description}</p>
+							<button className="home-btn">Rent this van</button>
+						</div>
+					</div>
+				) : (
+					<h2>Loading...</h2>
+				)}
+			</main>
 		</>
 	);
 }
