353a5a79fdccf165684748e3894f2a293222ab4a
SimoMassaro52 <simonesimomassaro@gmail.com>
2023-05-26 11:50:45 +0200

    random commit

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,51 +1,41 @@
-import { Link, useSearchParams, useLoaderData } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import "../../App.css";
-//Import of the async function that makes the fetch request in api.js
-import { getVans } from "../../api";
-
-export function loader() {
-	return getVans();
-}
 
 function Vans() {
-	// const [vansData, setVansData] = useState([]);
+	const [vansData, setVansData] = useState([]);
+
 	const [searchParams, setSearchParams] = useSearchParams();
+
 	const typeFilter = searchParams.get("type");
 
-	//We won't need to use a useEffect() anymore
-	const vansData = useLoaderData();
-	let filteredVans = [];
-	let vanElements = [];
+	useEffect(() => {
+		fetch("/api/vans")
+			.then((res) => res.json())
+			.then((data) => setVansData(data.vans));
+	}, []);
 
-	if (vansData.length > 0) {
-		filteredVans = typeFilter
-			? vansData.filter((van) => van.type === typeFilter)
-			: vansData;
-	}
+	const filteredVans = typeFilter
+		? vansData.filter((van) => van.type === typeFilter)
+		: vansData;
 
-	if (filteredVans.length > 0) {
-		vanElements = filteredVans.map((van) => (
-			<Link
-				to={van.id}
-				key={van.id}
-				state={{ search: searchParams.toString() }}
-			>
-				<div className="van-tile">
-					<img src={van.imageUrl} />
-					<div className="van-info">
-						<h3>{van.name}</h3>
-						<p>
-							${van.price}
-							<span>/day</span>
-						</p>
-					</div>
-					<i className={`van-type ${van.type}`}>
-						{van.type.charAt(0).toUpperCase() + van.type.slice(1)}
-					</i>
+	const vanElements = filteredVans.map((van) => (
+		<Link to={`/vans/${van.id}`} key={van.id}>
+			<div className="van-tile">
+				<img src={van.imageUrl} />
+				<div className="van-info">
+					<h3>{van.name}</h3>
+					<p>
+						${van.price}
+						<span>/day</span>
+					</p>
 				</div>
-			</Link>
-		));
-	}
+				<i className={`van-type ${van.type} selected`}>
+					{van.type.charAt(0).toUpperCase() + van.type.slice(1)}
+				</i>
+			</div>
+		</Link>
+	));
 
 	return (
 		<>
@@ -55,35 +45,23 @@ function Vans() {
 					<div className="explore-filters-box">
 						<button
 							onClick={() => setSearchParams({ type: "simple" })}
-							className={`filter-sim ${
-								typeFilter == "simple" ? "selected" : ""
-							}`}
+							className="filter-sim"
 						>
 							Simple
 						</button>
 						<button
 							onClick={() => setSearchParams({ type: "luxury" })}
-							className={`filter-lux ${
-								typeFilter == "luxury" ? "selected" : ""
-							}`}
+							className="filter-lux"
 						>
 							Luxury
 						</button>
 						<button
 							onClick={() => setSearchParams({ type: "rugged" })}
-							className={`filter-rug ${
-								typeFilter == "rugged" ? "selected" : ""
-							}`}
+							className="filter-rug"
 						>
 							Rugged
 						</button>
-						{typeFilter !== null ? (
-							<button className="clear-btn" onClick={() => setSearchParams({})}>
-								Clear filters
-							</button>
-						) : (
-							""
-						)}
+						<button onClick={() => setSearchParams({})}>Clear filters</button>
 					</div>
 				</div>
 				<div className="van-tiles-box">{vanElements}</div>
