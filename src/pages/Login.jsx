23bce144f42ddb0bd85d4a66bf388251ca04b1dd
SimoMassaro52 <simonesimomassaro@gmail.com>
2023-06-13 10:51:40 +0200

    added useNavigation and disabled button css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,13 @@
 import "../App.css";
 import { loginUser } from "../api";
-import { useState } from "react";
-import { useLoaderData, useNavigate, Form, redirect } from "react-router-dom";
+import {
+	useLoaderData,
+	useNavigate,
+	Form,
+	redirect,
+	useActionData,
+	useNavigation,
+} from "react-router-dom";
 
 //Code has been refactored after implementing action
 
@@ -13,37 +19,25 @@ export async function action({ request }) {
 	const formData = await request.formData();
 	const email = formData.get("email");
 	const password = formData.get("password");
-	const data = await loginUser({ email, password });
-	//To make a "better" fake auth we are going to save a boolean to local storage and check for it in requireAuth
-	localStorage.setItem("isLoggedIn", true);
-
-	console.log(data);
-	const response = redirect("/host");
-	response.body = true;
-	return response;
+	try {
+		const data = await loginUser({ email, password });
+		//To make a "better" fake auth we are going to save a boolean to local storage and check for it in requireAuth
+		localStorage.setItem("isLoggedIn", true);
+		const response = redirect("/host");
+		response.body = true;
+		return response;
+	} catch (err) {
+		return err.message;
+	}
 }
 
 export default function Login() {
+	//To show the login title according to where the loader loads from
 	const message = useLoaderData();
-
-	//This status state will be used to disable the log in button until the login actually happens
-	const [status, setStatus] = useState("idle");
-
-	//We are also setting an error state in order to display an error message if the user is not present in the database
-	const [error, setError] = useState(null);
-
-	//We are going to use the useNavigate() hook to redirect the user to the host page if the log in is successfull (switch isLoggedIn to true in utils.js)
-	const navigate = useNavigate();
-
-	// function handleSubmit(e) {
-	// 	setStatus("submitting");
-	// 	e.preventDefault();
-	// 		.then((data) => navigate("/host"))
-	// 		.catch((err) => setError(err))
-	// 		.finally(() => setStatus("idle"));
-	// 	setError(null);
-	// }
-
+	//To show some kind of error message if the log in fails
+	const errorMsg = useActionData();
+	//To show that the form is submitting the data
+	const navigation = useNavigation();
 	return (
 		<>
 			<main className="login-wrapper">
@@ -52,7 +46,7 @@ export default function Login() {
 				) : (
 					<h2>Sign in to your account</h2>
 				)}
-				{error && <h3>{error.message}</h3>}
+				{errorMsg && <h3>{errorMsg}</h3>}
 				<Form
 					method="post"
 					className="login-form"
@@ -62,8 +56,8 @@ export default function Login() {
 					<input type="email" placeholder="Email address" name="email" />
 					<input type="password" placeholder="Password" name="password" />
 					{/* Disabling the button depending on the state of the form submission */}
-					<button id="login-btn" disabled={status === "submitting"}>
-						{status === "submitting" ? "Logging in..." : "Log in"}
+					<button id="login-btn" disabled={navigation.state === "submitting"}>
+						{navigation.state === "submitting" ? "Logging in..." : "Log in"}
 					</button>
 				</Form>
 				<div>
